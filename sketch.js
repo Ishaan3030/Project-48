f0ab9217ae3383976992f22a1e310768facd8922
Ishaan3030 <86636860+Ishaan3030@users.noreply.github.com>
2021-10-08 17:24:10 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,91 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var background, bgImg;
+var runner1, run1img, runner2, run2img;
+var cone, cone1, cone2, cone3, cone4, cone5;
+var keyCode;
+
+function preload()
+{
+	bgImg = loadImage("./assets/track.jpg");
+  run1img = loadImage("./assets/runner1.gif");
+  run2img = loadImage("./assets/runner2.gif");
+  cone = loadImage("./assets/cone.png");
+  
+}
+
+function setup() {
+	createCanvas(2000, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    runner1 = createSprite(50, 200, 20, 20);
+    runner1.addImage(run1img);
+
+	runner2 = createSprite(50, 350, 20, 20);
+  runner2.addImage(run2img);
+  cone1 = createSprite(500, 100, 30, 30);
+  cone1.addImage(cone);
+
+  cone2 = createSprite(1000, 100, 30, 30);
+  cone2.addImage(cone);
+
+  cone3 = createSprite(1500, 100, 30, 30);
+  cone3.addImage(cone);
+
+  cone4 = createSprite(2000, 100, 30, 30);
+  cone4.addImage(cone);
+  
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(bgImg);
+
+  image(bgImg, 10, 0, width, height);
+
+  if(keyCode = "p"){
+    gameState = PLAY;
+  }
+
+  if(gameState === PLAY){
+    if(keyCode = RIGHT_ARROW){
+       runner1.velocityX = 3;
+    }
+
+    runner2.velocityX = 3;
+
+    if(runner1.x = 1950){
+      gameState = END;
+    }
+
+    if(gameState = END){
+      textSize(20);
+      text("YOU WIN", 700, 350);
+    }
+  }
+
+  
+  
+  drawSprites();
+ 
+}
+
+
+
